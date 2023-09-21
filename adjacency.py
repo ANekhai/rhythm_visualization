b4d8d3b82186393b142f3cc0fce4f1402a8e2be9
ANekhai <nekhai.anton@gmail.com>
2023-09-20 23:01:07 -0400

    Animated adjacency graph

--- adjacency.py
+++ adjacency.py
@@ -105,10 +105,66 @@ class CombiningRhythms(Scene):
         self.wait()
 
 
+class SequenceToGraph(Scene):
+    def construct(self):
+        # useful values
+        triple_buff = 3*DEFAULT_MOBJECT_TO_MOBJECT_BUFFER
+
+        # create three rhythm sequences
+        r1 = visualize_pattern(p1, YELLOW_A).scale(0.4).set_stroke(YELLOW)
+        r2 = visualize_pattern(p2, BLUE_A).scale(0.4).set_stroke(BLUE)
+        r3 = visualize_pattern(p3, MAROON_A).scale(0.4).set_stroke(MAROON)
+        r1.next_to(r2, LEFT, buff=triple_buff), r3.next_to(r2, RIGHT, buff=triple_buff)
+
+        # labels for rhythms
+        l1 = MarkupText(f'<span fgcolor="{YELLOW}">1</span>').next_to(r1, UP)
+        l2 = MarkupText(f'<span fgcolor="{BLUE}">2</span>').next_to(r2, UP)
+        l3 = MarkupText(f'<span fgcolor="{MAROON}">3</span>').next_to(r3, UP)
+        
+        # head and tail labels
+        h1 = MarkupText(f'<span fgcolor="{YELLOW}">1h</span>').next_to(r1, DL).shift(RIGHT)
+        t1 = MarkupText(f'<span fgcolor="{YELLOW}">1t</span>').next_to(r1, DR).shift(0.9*LEFT)
+        h2 = MarkupText(f'<span fgcolor="{BLUE}">2h</span>').next_to(r2, DL).shift(RIGHT)
+        t2 = MarkupText(f'<span fgcolor="{BLUE}">2t</span>').next_to(r2, DR).shift(0.9*LEFT)
+        h3 = MarkupText(f'<span fgcolor="{MAROON}">3h</span>').next_to(r3, DL).shift(RIGHT)
+        t3 = MarkupText(f'<span fgcolor="{MAROON}">3t</span>').next_to(r3, DR).shift(0.9*LEFT)
+        node_labels = [h1, t1, h2, t2, h3, t3]
+
+        # draw line to align labels on
+        start = r1.get_left()
+        end = r3.get_right()
+        line = Line(start, end)
+        num_points = 6
+        points = [line.point_from_proportion(i / (num_points - 1)) for i in range(num_points)]
+
+        # circles for labels
+        colors = ([YELLOW]*2) + ([BLUE]*2) + ([MAROON]*2)
+        node_circles = [Circle(radius=0.75).set_stroke(c).move_to(p) for c,p in zip(colors, points)]
+
+        # edges between nodes
+        e1 = Line(node_circles[1].get_right(), node_circles[2].get_left())
+        e2 = Line(node_circles[3].get_right(), node_circles[4].get_left())
+        e3 = ArcBetweenPoints(node_circles[0].get_bottom(), node_circles[5].get_bottom(), angle=1.4)
+        edges = [e1, e2, e3]
+
+        # animation
+        self.play(FadeIn(r1, r2, r3, l1, l2, l3))
+        self.wait(1)
+        self.play(FadeIn(*node_labels))
+        self.next_section("Dropping the rhythm, keeping the graph")
+        self.play(FadeOut(r1, r2, r3, l1, l2, l3))
+        moved_labels = [l.animate.move_to(p) for l,p in zip(node_labels,points)]
+        self.play(*moved_labels)
+        self.play(FadeIn(*node_circles))
+        self.wait(1)
+        self.play(FadeIn(*edges))
+
+
+
 class GraphCreation(Scene):
     def construct(self):
         k = 4  # number of adjacencies
-        theta = 2*PI/(2*k)  # distribute adjacencies evenly
+        theta = 2*PI/(2*k)  # distribute adjacencies evenly around circle
         
         # draw equidistant points along a circle
         circle = Circle(radius=3)
